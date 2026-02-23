c251f0894695201ba7ca7b4dbc0895b496223625
pratik <pratikkanzariya7@gmail.com>
2026-02-22 22:41:45 -0800

    commit

---
+++ module2/main (11).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+    void main()
+    { 
+        //area of circle = PI*r*r
+        
+        float PI = 3.14;
+        int r =5;
+        
+        float ans =PI*r*r;
+        printf("Area of circle is: %d",ans);
+        
+        getch();
+        
+    }    
