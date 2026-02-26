2c26f37b59bba522e585b2913a91c0c58a8c3a70
pratik <pratikkanzariya7@gmail.com>
2026-02-25 20:24:48 -0800

    commit

---
+++ module2/main (30).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+    {
+        int price =35000;
+        
+        if(price<30000)
+        {
+            printf("I can Purchase");
+        }
+        else
+        {
+            printf("I cant Purchase");
+        }
+        
+        return 0;
+    }
---
+++ module2/main (31).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+    {
+        int age =18;
+        
+        if(age>=18)
+        {
+            printf("Eligible to vote");
+        }
+        else
+        {
+            printf("Not Eligible to vote");
+        }
+        
+        return 0;
+    }
