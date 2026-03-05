3a33db12ca211555747f993e6b0631127812dfae
pratik <pratikkanzariya7@gmail.com>
2026-03-05 02:52:03 -0800

    commit

---
+++ MODULE 3.1/main (47).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("\n Name: pratik");
+    printf("\n Birth Date: 15 August 2000");
+    printf("\n Age: 25 years");
+    printf("\n Address: 123, Road, Rajkot, Gujarat");
+
+    return 0;
+}
