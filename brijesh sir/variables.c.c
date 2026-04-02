2d4aca65739d1d187380b6afe5edc519941a7b1c
pratik <pratikkanzariya7@gmail.com>
2026-04-01 22:02:56 -0700

    commit

---
+++ brijesh sir/variables.c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10,b=20,sum; //declartion of variables
+    sum=a+b;
+    printf("The sum of number is %d",sum); //printf()is an function i.e used to print the values of variables
+    return 0; //return 0 terminate a porgrame
+}
