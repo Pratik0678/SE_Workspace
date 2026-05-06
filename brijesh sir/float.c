840bdf31d90e7f25e537ec3a4d885dc527519c98
pratik <pratikkanzariya7@gmail.com>
2026-05-05 22:27:12 -0700

    commit

---
+++ brijesh sir/float.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float a=10.96;
+    
+    printf("The values of float is % .2f",a);
+    
+    return 0;
+}
---
+++ brijesh sir/formatter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age=35;
+    age;
+    int myage=45;
+    printf("My age is :%d",myage);
+    
+    return 0;
+}
